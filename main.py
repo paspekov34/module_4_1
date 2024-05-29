d77bbcf6849af1be831d1c99897daa845193c53a
paspekov34 <nikpaspekov17@mail.ru>
2024-05-29 22:54:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from fake_math import divide as div
+import true_math as tm
+
+
+result1 = div(84, 4)
+result2 = div(45, 0)
+result3 = tm.divide(14, 7)
+result4 = tm.divide(19, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+
+
+
